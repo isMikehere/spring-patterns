21a2aba8362251ce7d892aac8739581db749f195
Mike <mikelingtao@live.cn>
2021-01-13 23:47:53 -0500

    Client side load balance config

---
+++ reactive/src/main/kotlin/com/mlt/reactive/common/Logging.kt
@@ -0,0 +1,8 @@
+package com.mlt.reactive.common
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+interface Logging {
+    val logger: Logger get() = LoggerFactory.getLogger(javaClass.name)
+}
